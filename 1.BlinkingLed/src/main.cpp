0ee01f4f3e81c35267b731f26f0667348b8484d8
Rahul Soni <soni_rahul@live.com>
2023-12-01 19:48:39 +0530

    blinking led on arduino nano

--- 1.BlinkingLed/src/main.cpp
+++ 1.BlinkingLed/src/main.cpp
@@ -17,8 +17,8 @@ void loop()
 {
   digitalWrite(LED_BUILTIN, HIGH); // turn the LED on (HIGH is the voltage level)
   Serial.println("LED is ON");
-  delay(500);                    // wait for a second
+  delay(2000);                    // wait for a second
   digitalWrite(LED_BUILTIN, LOW); // turn the LED off by making the voltage LOW
   Serial.println("LED is OFF");
-  delay(500); // wait for a second
+  delay(2000); // wait for a second
 }
