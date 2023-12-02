1d3be86aeec2de5bde9d1a6345632b4ad78f0fc3
Rahul Soni <soni_rahul@live.com>
2023-12-02 17:00:00 +0530

    blinking Led on ESP32

--- 1.BlinkingLed/src/main.cpp
+++ 1.BlinkingLed/src/main.cpp
@@ -1,6 +1,10 @@
 #include <Arduino.h>
 
+#ifdef ARDUINO_ARCH_ESP32
+#define MONITOR_SPEED 115200
+#else
 #define MONITOR_SPEED 9600
+#endif
 
 // the setup function runs once when you press reset or power the board
 void setup()
@@ -17,8 +21,8 @@ void loop()
 {
   digitalWrite(LED_BUILTIN, HIGH); // turn the LED on (HIGH is the voltage level)
   Serial.println("LED is ON");
-  delay(2000);                    // wait for a second
+  delay(100);                    // wait for a second
   digitalWrite(LED_BUILTIN, LOW); // turn the LED off by making the voltage LOW
   Serial.println("LED is OFF");
-  delay(2000); // wait for a second
+  delay(100); // wait for a second
 }
